d6788c1a5fcc80bb5b5fec06ba25c04883cc14ca
Guruprasad Zapate <guruprasad.zapate@siemens.com>
2019-08-21 15:30:22 +0200

    update book list template to add search filter

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" v-model="searchInput" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <h2>Filtered Books by Ownership</h2>
@@ -37,7 +38,8 @@ export default {
         { title: "Amusing Ourselves to Death", author: "Neil Postman",finishedReading: true,ownership:"bought" }
       ],
       filters:["bought","borrowed"],
-      holding:"bought"    
+      holding:"bought",
+      searchInput:"" 
     };
   },
   components: {
@@ -47,6 +49,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books,["ownership",this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      }
+      return _.filter(this.books,searchFilter);
     }
   },
   methods: {
